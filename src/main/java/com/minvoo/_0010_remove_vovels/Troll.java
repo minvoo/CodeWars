2509d68a88fa11d3c85978d75c9ce219d00a60cd
minvoo <maciejewski_mariusz@wp.pl>
2022-11-27 09:54:20 +0100

    kata 0010

---
+++ src/main/java/com/minvoo/_0010_remove_vovels/Troll.java
@@ -0,0 +1,10 @@
+package com.minvoo._0010_remove_vovels;
+
+public class Troll {
+
+    public static String disemvowel(String str) {
+
+    return str.replaceAll("[eEuUiIoOaA]", "");
+
+    }
+}
---
+++ src/test/java/com/minvoo/_0010_remove_vovels/TrollTest.java
@@ -0,0 +1,20 @@
+package com.minvoo._0010_remove_vovels;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrollTest {
+
+
+    @Test
+    public void shouldRemoveVowels() {
+
+        assertEquals("Ths wbst s fr lsrs LL!",Troll.disemvowel("This website is for losers LOL!")
+        );
+        assertEquals("N ffns bt,\nYr wrtng s mng th wrst 'v vr rd", Troll.disemvowel(
+                "No offense but,\nYour writing is among the worst I've ever read"));
+        assertEquals( "Wht r y,  cmmnst?", Troll.disemvowel("What are you, a communist?"));
+    }
+
+}
